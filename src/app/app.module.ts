9e9ffc8c8d423f2e8c869a2f00bdb9ce55b3b84e
Fransiskaindri <fransiskaindri15@gmail.com>
2022-01-10 14:41:35 +0700

    Project-spa Frans

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AppHeaderComponent } from './app-header/app-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AppHeaderComponent
   ],
   imports: [
     BrowserModule,
